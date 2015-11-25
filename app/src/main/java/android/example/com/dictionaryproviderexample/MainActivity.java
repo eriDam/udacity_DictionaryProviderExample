ad082da74028132febffded3aab2011eed866f1e
eriDam <erika.c.caballero@gmail.com>
2015-11-25 01:12:49 +0100

    4.03 Solution Add Cursor adapter with listView

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -29,8 +29,16 @@ import android.support.v4.widget.SimpleCursorAdapter;
  * to show an example of accessing the {@link Words} list via its' Content Provider.
  */
 public class MainActivity extends ActionBarActivity {
+    // For the SimpleCursorAdapter to match the UserDictionary columns to layout items.
+    private static final String[] COLUMNS_TO_BE_BOUND  = new String[] {
+            UserDictionary.Words.WORD,
+            UserDictionary.Words.FREQUENCY
+    };
 
-
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[] {
+            android.R.id.text1,
+            android.R.id.text2
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,23 +46,23 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         // Get the TextView which will be populated with the Dictionary ContentProvider data.
-        //TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
-        //4.03
         ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
-        // Get the ContentResolver which will send a message to the ContentProvider
+
+        // Get the ContentResolver which will send a message to the ContentProvider.
         ContentResolver resolver = getContentResolver();
 
-        // Get a Cursor containing all of the rows in the Words table
+        // Get a Cursor containing all of the rows in the Words table.
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
-
-
         // Set the Adapter to fill the standard two_line_list_item layout with data from the Cursor.
-        SimpleCursorAdapter adapter = null;
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.two_line_list_item,
+                cursor,
+                COLUMNS_TO_BE_BOUND,
+                LAYOUT_ITEMS_TO_FILL,
+                0);
 
-
+        // Attach the adapter to the ListView.
+        dictListView.setAdapter(adapter);
     }
-
-
-    }
-
+}
